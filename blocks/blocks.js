c21b21a2ccbd317fb95960759601f1ce2a4fa201
Mikko Meriläinen <mikko@merilainen>
2024-11-25 18:14:06 +0200

    Add mean, improve scaling & websocket tests

--- blocks/blocks.js
+++ blocks/blocks.js
@@ -1,6 +1,6 @@
 "use strict";
 
-import express from 'express'
+import express from 'express';
 import { WebSocketServer } from 'ws';
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
@@ -8,8 +8,8 @@ import { join, dirname } from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-const app = express()
-const port = 8080
+const app = express();
+const port = 8080;
 
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, "blocks.html"));
@@ -21,7 +21,7 @@ app.get('/http_test', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 })
 
 const wss = new WebSocketServer({ port: 8081 });
@@ -30,10 +30,10 @@ wss.on('connection', function connection(ws) {
   ws.on('error', console.error);
   ws.on('message', function message(data) {
     if (data.toString('utf8') === 'ws_test') {
-      ws.send("ws_test_response")
+      ws.send("ws_test_response");
     }
     else if (data.toString('utf8') === 'ws_test_continuous') {
-      ws.send("ws_test_continuous_response")
+      ws.send("ws_test_continuous_response");
     }
     console.log(`Received WebSocket data: ${data}`);
   });
